8db3aa1ff388bee47c16913196cf9b0a1b834a9c
Robert Peteuil <robert@peteuil.com>
2017-12-01 15:34:19 -0700

    Update message from demo-app in Alpine Linux

--- python2.7-alpine/app/main.py
+++ python2.7-alpine/app/main.py
@@ -1,3 +1,4 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello World from a default uWSGI Python 2.7 app in an alpine Docker container with Nginx (default)"]
+    return ["Hello World from a default uWSGI Python 2.7 app in an\
+            Alpine-Linux Docker container with Nginx (default)"]
